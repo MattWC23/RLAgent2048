fdd58152d391a06102e36225f519d97424b3e748
Matt Cowan <matt@Matts-MacBook-Air.local>
2026-01-13 19:01:21 -0800

    simulation, backpropogation, root expansion completed

--- montecarlo.py
+++ montecarlo.py
@@ -1,26 +1,31 @@
 
 from game import Game, game_over
+from typing import Dict, Set
 import random
 import math
 
 
 class EdgeStats:
     def __init__(self):
-        self.visits = 0
-        self.total_value = 0
+        self.visits: int = 0
+        self.total_value: float = 0
     
-    def action_value(self):
+    def action_value(self) -> float:
         return self.total_value / self.visits if self.visits > 0 else 0.0
 class StateNode:
+    visit_count : int
+    edges : Dict[str, EdgeStats]
+    children : Dict[str, Set[tuple]]
+    key : tuple
     def __init__(self, board_key, visit_count=0, children=None):
         self.visit_count = visit_count
-        self.edges = {} #action -> EdgeStats
-        self.children = children if children else {} #action -> set(state_key)
+        self.edges = {'UP': set(), 'DOWN' : set(), 'LEFT' : set(), 'RIGHT' : set()} #action -> EdgeStats
+        self.children = children if children is not None else {} #action -> set(state_key)
         self.key = board_key
         self.untried = ['UP', 'DOWN', 'LEFT', 'RIGHT']
     
     def add_child(self, move, board_key):
-        self.children[move].add(board_key)
+        self.children[move].setdefault(move, set()).add(board_key)
     
     def utc_action(self, C=1.2):
         """
@@ -30,7 +35,7 @@ class StateNode:
         """
         best_action = None
         best_UTC = float('-inf')
-        for action, edge in self.edges.entries():
+        for action, edge in self.edges.items():
             if edge.action_value() + C * math.sqrt(math.log(self.visit_count) / edge.visits) > best_UTC:
                 best_action = action
             elif edge.action_value() + C * math.sqrt(math.log(self.visit_count) / edge.visits) == best_UTC:
@@ -58,21 +63,9 @@ class MCTS():
         """
         pass
 
-    # def compute_mcts(self, game : Game):
-    #     #First, find valid moves
-    #     moves = [move for move in ['UP', 'DOWN', 'LEFT', 'RIGHT'] if game.can_move(move)]
-    #     #We need to compute and store the overall reward for starting with a certain move across all simulations
-    #     action_values = {}
-    #     for move in moves:
-    #         new_game = Game(game.board)
-    #         reward = new_game.move(move)
-    #         reward += self.play_random(new_game, self.SIMS, action_values)
-    #         action_values[(self.hashable(new_game.board), move)] = reward
-    #     for _ in range(self.ROLL_OUTS - len(moves)):
-    #         #Do some kind of UTC calculation to pick inital move
-    #         self.play_random() #finish later, simulate the rest randomly
-    #     #later return argmax
-    #     return None
+    def compute_mcts(self):
+        self._expand_tree()
+
 
     def _expand_tree(self):
         """"
@@ -87,8 +80,9 @@ class MCTS():
             self.nodes[new_board_key] = StateNode(new_board_key, visit_count=1)
             self.root.edges[move].total_value += move_reward
             self.root.edges[move].visits += 1
-            path = self.play_random()
+            path = self.play_random(self.game, self.SIMS)
             self.back_prop(path)
+            self._reset()
 
 
 
@@ -96,33 +90,44 @@ class MCTS():
     def play_random(self, game : Game, num_iter : int):
         """
         Plays a random policy and returns a list path of board_keys corresponding to nodes in self.nodes
-        in the order they were traversed
+        in the order they were traversed. The first node in the path is the initial state given before
+        any random moves are played. Eeach item in the path is (direction of indegree edge, node state, value of moving).
+        An example path is [(None, s1, 0), ('RIGHT', s2, 3.2), ...., ('UP', sn, 34)]
         """
-        path = [game.key()]
+        path = [(None, game.key(), 0)]
         for _ in range(num_iter):
-            playable_moves = [move for move in ['UP', 'DOWN', 'LEFT', 'RIGHT'] if self.game.can_move(move)]
+            playable_moves = [move for move in ['UP', 'DOWN', 'LEFT', 'RIGHT'] if game.can_move(move)]
             idx = random.randint(0, len(playable_moves) - 1)
             move = playable_moves[idx]
-            move_reward = self.game(move)
-            new_board_key = self.game.key()
+            move_reward = game.move(move)
+            new_board_key = game.key()
             self.nodes[path[-1]].add_child(move, new_board_key)
             if new_board_key in self.nodes:
                 self.nodes[new_board_key].visit_count += 1
             else:
                 self.nodes[new_board_key] = StateNode(new_board_key, visit_count=1)
-            self.nodes[path[-1]].edges[move] += move_reward
-            self.nodes[path[-1]].edges(move) += 1
-            path.append(new_board_key)
+            self.nodes[path[-1][1]].edges[move].total_value += move_reward
+            self.nodes[path[-1][1]].edges[move].visits += 1
+            path.append((move,new_board_key, move_reward))
+            if game.isTerminated():
+                break
         
         return path
     
 
     def back_prop(self, path):
-        pass
+        n = len(path)
+        for i in range(n - 1, 0, -1):
+            move, _, reward = path[i]
+            parent = path[i - 1][1]
+            edge = self.nodes[parent].edges[move]
+            edge.total_value += reward
+            edge.visits += 1
+            self.nodes[parent] += 1
 
     
     def _reset(self):
         """
-        Resets the internal game used to simulate play to be in the state the search tree was initialized in
+        Resets the internal game used to simulate play to be in the state the search tree was initialized with
         """
-        self.game = Game(self.root.board_key)
+        self.game = Game(self.root.key)
